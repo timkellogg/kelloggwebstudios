d97db1359d41ab7ae231aaf368165e8a82bf77de
timkellogg <tim.kellogg@gmail.com>
2015-04-29 12:17:52 -0700

    fix contact.rb syntax error

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,7 +1,7 @@
 class Contact < MailForm::Base
 	attribute :name, 		:validate => true 
 	attribute :email,		:validate => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	attribute :message,		
+	attribute :message	
 	attribute :nickname,	:captcha => true 
 
 	def headers
